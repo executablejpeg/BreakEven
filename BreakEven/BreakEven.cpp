9a2fe263489dba1e4e06c24867f9a3fbeb18472c
samhuseman <husemansam@gmail.com>
2019-02-21 17:05:52 -0500

    this is the federal tax calculator

--- BreakEven/BreakEven.cpp
+++ BreakEven/BreakEven.cpp
@@ -4,10 +4,79 @@
 #include "pch.h"
 #include <iostream>
 
+
 int main()
 {
-	int i = 5;
-    std::cout << "Hello World!\n"; 
+	double income = 0;
+	double taxRate = 0;
+	double add = 0;
+	double subtract = 0;
+	double incomeTax = 0;
+	string taxStatus = "";
+	string file = "";
+	string answer = "";
+top:
+	cout << "\nPlease enter your income to calculate your taxes\n " << endl;
+	cin >> income;
+	while (!(cin >> income)) //get input
+	{
+		if (isdigit(income))
+		{
+			break;
+		}
+
+		else
+		{
+			//if input fails, run this
+			cin.clear();
+			cin.sync();
+			cout << "\nSorry, that was not a valid number. Please enter a valid number\n ";
+		}
+	}
+	cin.ignore(80, '\n');
+
+	int main()
+	{
+		if (income <= 9525)
+		{
+			taxRate = .1;
+			subtract = 0;
+			add = 0;
+		}
+
+		if (income >= 9526 & income <= 38700)
+		{
+			taxRate = .12;
+			subtract = 9525;
+			add = 25;
+		}
+
+		if (income >= 38701 & income <= 82500)
+		{
+			taxRate = .22;
+			subtract = 38700;
+			add = 85;
+		}
+
+		if (income > 4313)
+		{
+			taxRate = .075;
+			subtract = 4313;
+			add = 181;
+		}
+	}
+
+	incomeTax = ((income - subtract) * taxRate) + add;
+
+	cout << "\nYour taxable income is " << income << endl;
+
+	}
+
+	cout << "\nThat means your income tax will be " << incomeTax << endl;
+
+	system("PAUSE");
+
+	return 0;
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
