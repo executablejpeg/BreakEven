d5613982b33070933c3b9c9678bbfd37001701a9
KeetonKetron <keeton5488@gmail.com>
2019-02-21 16:52:39 -0500

    added OutFile

---
+++ BreakEven/OutFile.cpp
@@ -0,0 +1,22 @@
+#include "OutFile.h"
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+void OutputData(send)
+{
+	ofstream OutFile;
+	OutFile.open("InformationData.txt");
+	ifstream InFile;
+	InFile.open("InputData.txt");
+	
+	string example;
+
+	InFile >> example;
+
+	OutFile << send << endl << example;
+
+	InFile.close();
+	OutFile.close();
+}
